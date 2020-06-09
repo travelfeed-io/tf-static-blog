f00157358c97898503a78dc73c5ecce57f86092a
tiotdev <tiotdev@tiot.de>
2020-06-09 19:12:34 +0200

    Add viewpoint meta for correct mobile scaling

--- components/Layout/Wrapper.js
+++ components/Layout/Wrapper.js
@@ -1,9 +1,16 @@
+import NextHead from 'next/head';
 import Footer from './Footer';
 import Header from './Header';
 
 export default function Jumbotron({ children }) {
   return (
     <>
+      <NextHead>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </NextHead>
       <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
         <Header />
         <main
