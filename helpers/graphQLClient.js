355f5928382b1c5c98525b95f4565908a8276fab
tiotdev <tiotdev@tiot.de>
2020-06-09 18:40:00 +0200

    Deploy to vercel

--- helpers/graphQLClient.js
+++ helpers/graphQLClient.js
@@ -4,7 +4,7 @@ import { print } from 'graphql/language/printer';
 const graphQLClient = new GraphQLClient('https://api.travelfeed.io/graphql', {
   credentials: 'same-origin',
   headers: {
-    Authorization: process.env.API_KEY,
+    Authorization: process.env.TF_API_KEY,
   },
 });
 
